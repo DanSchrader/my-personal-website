8764e3f30e7dd01c49de2ff9ecd2e103b80bf2f4
DanSchrader <dhs@schrader-wolff-media.de>
2022-04-28 13:59:59 +0200

    Add more content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,35 @@
 import styled from 'styled-components';
-import BDLogo from '../images/beerserker-design-logo.svg'
+import BDLogo from '../images/beerserker-design-logo.svg';
+import BPLogo from '../images/beerserker-productions-logo.svg';
+import BILogo from '../images/burns-iceman-logo.svg';
 
 export default function NavBar () {
     return <NavBarWrapper>
         <BDLogoWrapper src={BDLogo} alt='Beerserker-Design-Logo' />
-        <Title>Beerserker Design - Daniel Schrader</Title>
+        <BPLogoWrapper src={BPLogo} alt='Beerserker-Productions-Logo' />
+        <BILogoWrapper src={BILogo} alt='Burns-and-Iceman-Logo' />
+        <Title>Daniel Schrader</Title>
     </NavBarWrapper>
 }
 
 const NavBarWrapper = styled.header`
 display: grid;
-grid-template-columns: 48px auto;
+grid-template-columns: 48px 48px 48px auto;
 gap: 10px;
 align-items: center;
 `
 
-const BDLogoWrapper=styled.img`
+const BDLogoWrapper = styled.img`
+display: grid;
+width: 48px;
+`
+
+const BPLogoWrapper = styled.img`
+display: grid;
+width: 48px;
+`
+
+const BILogoWrapper = styled.img`
 display: grid;
 width: 48px;
 `
@@ -26,4 +40,5 @@ margin: 0;
 padding: 0;
 font-size: 200%;
 font-weight: 300;
+text-align: center;
 `
